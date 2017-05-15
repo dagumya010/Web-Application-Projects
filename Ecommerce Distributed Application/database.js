551574b8730c8f44264831d690372a1a303dfff1
dagumya <agumya.d@husky.neu.edu>
2017-05-15 15:28:24 -0700

    Overview and removal of passwords

--- Ecommerce Distributed Application/database.js
+++ Ecommerce Distributed Application/database.js
@@ -2,29 +2,12 @@ var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
 
-// var pool = mysql.createPool({
-//     connectionLimit: 100,
-//     host: 'myinstance.cnkvfbeumu7f.us-west-2.rds.amazonaws.com',
-//     user: 'davidagumya',
-//     password: 'joseph1234',
-//     database: 'ecommerceapplication'
-// });
-
-// Test Database
-// var pool = mysql.createPool({
-//     connectionLimit: 100,
-//     host: 'secondarydb.cnkvfbeumu7f.us-west-2.rds.amazonaws.com',
-//     user: 'davidagumya',
-//     password: 'joseph1234',
-//     database: 'ecommerceapplication'
-// });
-// Bigger DataBase
 var pool = mysql.createPool({
     connectionLimit: 100,
-    host: 'thirddatabase.cnkvfbeumu7f.us-west-2.rds.amazonaws.com',
-    user: 'davidagumya',
-    password: 'joseph1234',
-    database: 'ecommerceapplication'
+    host: '', // aws RDS host
+    user: '', // username for the aws instance
+    password: '', // password for the aws instance
+    database: '' // schema
 });
 module.exports = pool;
 
